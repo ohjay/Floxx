6bbe95528642fa4bc379612820c5663e4f101361
Owen Jow <owenjow@berkeley.edu>
2016-04-30 14:58:32 -0700

    Resolved the activity_activity error

--- app/src/main/java/co/floxx/floxx/ActivityFriendList.java
+++ app/src/main/java/co/floxx/floxx/ActivityFriendList.java
@@ -12,7 +12,19 @@ public class ActivityFriendList extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.content_activity_friend_list);
+        setContentView(R.layout.activity_activity_friend_list);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
 
 }
