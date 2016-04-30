ddd6c1aedefd096346c47efddf234f537b2bbcd0
Owen Jow <owenjow@berkeley.edu>
2016-04-30 15:40:44 -0700

    Configured fl activity for viewing

--- app/src/main/java/co/floxx/floxx/ActivityFriendList.java
+++ app/src/main/java/co/floxx/floxx/ActivityFriendList.java
@@ -14,9 +14,6 @@ public class ActivityFriendList extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activity_friend_list);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
